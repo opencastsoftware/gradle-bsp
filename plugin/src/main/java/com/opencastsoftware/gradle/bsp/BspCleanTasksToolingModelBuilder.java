6fe4edba3b6be3955bd1d7c8dc3f5fad5b3ebecd
David Gregory <2992938+DavidGregory084@users.noreply.github.com>
2023-08-11 12:03:10 +0100

    Don't use StringUtils capitalize in plugin implementation

--- plugin/src/main/java/com/opencastsoftware/gradle/bsp/BspCleanTasksToolingModelBuilder.java
+++ plugin/src/main/java/com/opencastsoftware/gradle/bsp/BspCleanTasksToolingModelBuilder.java
@@ -8,7 +8,6 @@ import com.opencastsoftware.gradle.bsp.model.BspCleanTasks;
 import com.opencastsoftware.gradle.bsp.model.DefaultBspCleanTasks;
 import org.gradle.api.Project;
 import org.gradle.api.plugins.JavaPluginExtension;
-import org.gradle.internal.impldep.org.apache.commons.lang.StringUtils;
 import org.gradle.language.base.internal.plugins.CleanRule;
 import org.gradle.language.base.plugins.LifecycleBasePlugin;
 import org.gradle.tooling.provider.model.ToolingModelBuilder;
@@ -20,6 +19,10 @@ public class BspCleanTasksToolingModelBuilder extends BspModelBuilder implements
     @Override
     public boolean canBuild(String modelName) {
         return modelName.equals(BspCleanTasks.class.getName());
+    }
+
+    private String capitalise(String taskName) {
+        return Character.toTitleCase(taskName.charAt(0)) + taskName.substring(1);
     }
 
     @Override
@@ -34,7 +37,7 @@ public class BspCleanTasksToolingModelBuilder extends BspModelBuilder implements
                     var classesTaskName = sourceSet.getClassesTaskName();
                     var classesTask = project.getTasks().findByName(classesTaskName);
                     Optional.ofNullable(classesTask).ifPresent(classes -> {
-                        var cleanTaskName = CleanRule.CLEAN + StringUtils.capitalize(classesTaskName);
+                        var cleanTaskName = CleanRule.CLEAN + capitalise(classesTaskName);
                         var cleanTask = project.getTasks().findByName(cleanTaskName);
                         Optional.ofNullable(cleanTask).ifPresent(clean -> {
                             cleanTasks.put(sourceSetTargetId, clean.getPath());
